d9649bd533a57b8ecca6794278b04b062b8593da
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-01-04 20:59:53 +0800

    Added base files

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# api/proxy
-> proxy for several APIs
-
-### API list can be found at the root route where you will be greeted with JSON which will tell you the enpoint and also the params
+# raspi-iot-bus
 
 ## Run
 ```bash
+# CHANGE INTERNAL IP IN SERVER.JS (VARIABLE NAME IS INTERNAL IP) (FIND OUT BY IFCONFIG)
 npm install
 node server.js
 # Then, go to localhost:5000 or you can change it on your own
 ```
+
+## Then visit in the browser (on the same WiFi Network):
+-   {internal_ip}:5000/busdisplay/{busstopcode}
+    -   e.g: 192.168.0.0:5000/busdisplay/27301
+
+## Expected Output On Raspberry Pi
+-   example:
+    -   179 (4 Mins)
--- server.js
+++ server.js
@@ -1,120 +1,75 @@
-var express = require('express');
-var fetch = require('node-fetch');
-var app = express();
-const PORT = process.env.PORT || 5000
+let express = require('express');
+let fetch = require('node-fetch');
+let app = express();
+let sense = require("sense-hat-led");
 
+let internal_ip = "192.168.0.0"
+
+let color = [255, 255, 255]
+
+// GET ROOT PAGE
 app.get('/', (request, response) => {
+    response.send('HELLO THERE!!')
+})
+
+// CLEAR
+app.get('/clear', (request, response) => {
+
+    // CLEAR THE SENSE HAT DISPLAY
+    sense.clear();
+
+    // SEND
     response.send({
-
-        port: PORT,
-
-        github: 'https://github.com/renabil/api',
-        
-        api: {
-            'Reddit': {
-                endpoint: '/api/reddit/:subreddit/:limit?',
-                params: {
-                    subreddit: 'Preferred subreddit',
-                    limit: 'max 100, default 25'
-                }
-            },
-            'Bus Stops (By Code)': {
-                endpoint: '/api/bus/stop/code/:busstopnumber?',
-                params: {
-                    busstopnumber: 'optional, returns all bus stops if the param is not inside the link'
-                }
-            },
-
-            'Bus Stops (By Name)': {
-                endpoint: '/api/bus/stop/name/:busstopname?',
-                params: {
-                    busstopname: 'optional, returns all bus stops if the param is not inside the link'
-                }
-            }
-        }
+        error: false,
     })
+
 })
 
-// REDDIT
-app.get('/api/reddit/:subreddit/:limit?', (request, response) => {
-    var subreddit = request.params.subreddit;
-    var limit = request.params.limit;
+app.get('/busdisplay/:stopnumber', (request, response) => {
 
-    if (limit) {
-        fetch('https://www.reddit.com/r/' + subreddit + '/new.json?limit=' + limit)
+    // CLEAR THE DISPLAY FIRST
+    sense.clear()
+
+    let stopnumber = request.params.stopnumber;
+    let index = 0;
+
+    function start() {
+
+        // fetch
+        fetch('http://arrivelah.herokuapp.com/?id=' + stopnumber)
             .then(response => response.json())
             .then(json => {
-                response.send(json)
+
+                // speed
+                let speed = 0.1
+
+                // show the message
+                // the start at the end is the loop callback
+                sense.showMessage(`${json.services[index].no} (${Math.floor(json.services[index].next.duration_ms / 60000)} Mins)`, speed, color, start);
+
+
+                // if longer than services number get it to 0
+                if (index >= json.services.length - 1) {
+                    index = 0;
+                }
+
+                // increment
+                index++
+
             })
-    } else {
-        fetch('https://www.reddit.com/r/' + subreddit + '/new.json')
-            .then(response => response.json())
-            .then(json => {
-                response.send(json)
-            })
+
     }
 
-})
+    // invoke the start function
+    start()
 
-// BUS STOPS (CODE)
-app.get('/api/bus/stop/code/:busstopnumber?', (request, response) => {
-
-    var busstopnumber = request.params.busstopnumber;
-
-    if (busstopnumber) {
-        fetch('https://busrouter.sg/data/2/bus-stops.json')
-            .then(response => response.json())
-            .then(json => {
-                var rtnvalue = json.filter(data => {
-                    return data.no.match(busstopnumber)
-                })
-
-                response.send(rtnvalue)
-            })
-    } else {
-        fetch('https://busrouter.sg/data/2/bus-stops.json')
-            .then(response => response.json())
-            .then(json => {
-                var rtnvalue = json.filter(data => {
-                    return data.no.match()
-                })
-
-                response.send(rtnvalue)
-            })
-    }
+    // SEND
+    response.send({
+        stopnumber: stopnumber,
+        error: false,
+    })
 
 })
 
-// BUS STOPS (NAME)
-app.get('/api/bus/stop/name/:busstopname?', (request, response) => {
-
-    var busstopname = request.params.busstopname.toLowerCase();
-
-    if (busstopname) {
-        fetch('https://busrouter.sg/data/2/bus-stops.json')
-            .then(response => response.json())
-            .then(json => {
-                var rtnvalue = json.filter(data => {
-                    return data.name.toLowerCase().match(busstopname)
-                })
-
-                response.send(rtnvalue)
-            })
-    } else {
-        fetch('https://busrouter.sg/data/2/bus-stops.json')
-            .then(response => response.json())
-            .then(json => {
-                var rtnvalue = json.filter(data => {
-                    return data.name.toLowerCase().match()
-                })
-
-                response.send(rtnvalue)
-            })
-    }
-
-})
-
-
-app.listen(PORT, () => {
-    console.log('Server is running!')
-})
+// LISTEN ON PORT 5000 ON INTERNAL IP
+app.listen(5000, internal_ip)
